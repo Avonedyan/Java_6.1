3d67098160cf135cd072c5d1498f08c895e068dd
Polina Naydenova <avonedyan@gmail.com>
2023-02-01 20:48:47 -0800

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ import ru.netology.sqr.Java61.services.SQRService;
 
 public class Main {
     public static void main(String[] args) {
-        SQRService service = new SQRService();
-        int mySQR = service.sqrService(200, 700);
+        int mySQR = SQRService.sqrService(200, 700);
         System.out.println(mySQR);
     }
 }
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -6,49 +6,43 @@ public class SQRServiceTest {
 
     @Test
     public void testPositiveMinAndMax() {
-        SQRService service = new SQRService();
         int expected = 12;
-        int actual = service.sqrService(200, 700);
+        int actual = SQRService.sqrService(200, 700);
        Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testNegativeMinAndMax() {
-        SQRService service = new SQRService();
         int expected = 0;
-        int actual = service.sqrService(-200, -700);
+        int actual = SQRService.sqrService(-200, -700);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testPositiveMinNegativeMax() {
-        SQRService service = new SQRService();
         int expected = 0;
-        int actual = service.sqrService(200, -700);
+        int actual = SQRService.sqrService(200, -700);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testNegativeMinPositiveMax() {
-        SQRService service = new SQRService();
         int expected = 17;
-        int actual = service.sqrService(-200, 700);
+        int actual = SQRService.sqrService(-200, 700);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void testEqualMinAndMax() {
-        SQRService service = new SQRService();
         int expected = 1;
-        int actual = service.sqrService(100, 100);
+        int actual = SQRService.sqrService(100, 100);
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     public void test0() {
-        SQRService service = new SQRService();
         int expected = 0;
-        int actual = service.sqrService(0, 0);
+        int actual = SQRService.sqrService(0, 0);
         Assertions.assertEquals(expected, actual);
     }
 }
